eecaca3e8763653892adb048e40fe74a33691a40
a.gribov <llununull@gmail.com>
2025-01-21 00:21:30 +0300

    feat: use HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import ScrollToTop from './components/ScrollToTop';
 import AllCats from './pages/AllCats';
 import FavoriteCats from './pages/FavoriteCats';
 import RouteTitleUpdater from './components/RouteTitleUpdater';
 
-const App: React.FC = () => {
-  const basename: string = import.meta.env.MODE === 'production' ? '/uchiru-catinterest/' : '/';
-
-  return (
-    <Router basename={basename}>
-      <ScrollToTop />
-      <RouteTitleUpdater />
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<AllCats />} />
-          <Route path="/favorites" element={<FavoriteCats />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <ScrollToTop />
+    <RouteTitleUpdater />
+    <Header />
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<AllCats />} />
+        <Route path="/favorites" element={<FavoriteCats />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 export default App;
